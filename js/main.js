b9d9ed0085b9d2a15d85216fe287880480d891a7
Bhargava Peddapudi <bpeddapudi@equinix.com>
2017-07-12 15:09:07 -0700

    Second Commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(".flip-card").flip({
+  axis: 'x',
+  trigger: 'hover',
+  reverse: true
+});
