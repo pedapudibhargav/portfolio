f0756ba21e116ed097c839a4aed5ede048ef6c93
bhargav peddapudi <pedapudibhargav@gmail.com>
2017-07-14 20:22:16 -0700

    no message

--- js/main.js
+++ js/main.js
@@ -20,7 +20,22 @@ $( document ).ready(function() {
       setTimeout(function(){
         console.log("set time out triggered");
         $(curtTile).click();
+        if(index == 6)
+        {
+          randomTiles();
+        }
       }, (index + 1)*200);
     });
+    var ct = 0;
+    var randomTiles = function(){
+      setTimeout(function(){
+        $( ".flip-card:eq( "+Math.random() * (7 -0) + 0; +")" ).click();
+          if(ct < 3)
+            {
+              randomTiles();
+              ct++;
+            }
+        },200);
 
+    };
 });
