88f650abf2f9fdfb2bc904db87ebb341ae3372d6
Bhargava Peddapudi <bpeddapudi@equinix.com>
2017-07-20 13:54:04 -0700

    Skills UI updated

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.carousel').carousel();
+    $('.tooltipped').tooltip({delay: 50});
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
