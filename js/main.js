95a7d628424b341861f55be1633f232e7e577265
Bhargava Peddapudi <bpeddapudi@equinix.com>
2017-07-20 18:43:23 -0700

    Typewriter Logic Integrated

--- js/main.js
+++ js/main.js
@@ -1,52 +1,67 @@
 (function($){
   $(function(){
+    var flipTiles = function(){
+      $( ".flip-card" ).each(function( index ) {
+        var curtTile = $(this);
+        setTimeout(function(){
+          console.log("set time out triggered");
+          $(curtTile).click();
+          if(index == 6)
+          {
+            randomTiles();
+          }
+        }, (index + 1)*200);
+      });
+    };
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.carousel').carousel();
     $('.tooltipped').tooltip({delay: 50});
     $('.modal').modal();
-
-    // Select all links with hashes
-      $('a[href*="#"]')
-        // Remove links that don't actually link to anything
-        .not('[href="#"]')
-        .not('[href="#0"]')
-        .click(function(event) {
-          // On-page links
-          if (
-            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-            &&
-            location.hostname == this.hostname
-          ) {
-            // Figure out element to scroll to
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            // Does a scroll target exist?
-            if (target.length) {
-              // Only prevent default if animation is actually gonna happen
-              event.preventDefault();
-              $('html, body').animate({
-                scrollTop: target.offset().top
-              }, 1000, function() {
-                // Callback after animation
-                // Must change focus!
-                var $target = $(target);
-                $target.focus();
-                if ($target.is(":focus")) { // Checking if the target was focused
-                  return false;
-                } else {
-                  $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                  $target.focus(); // Set focus again
-                };
-              });
-            }
+    $('a[href*="#"]')
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+          &&
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
           }
-        });
+        }
+      });
      $('.proj-preview').click(function(event){
        console.log("Updating preview video"+$(this).attr("data-url"));
        $("#proj-mdl-iframe").attr('src',$(this).attr("data-url"));
      })
+    $('.say-hi').click(function(){
+      $(".typewriter-outer").fadeOut(400, function(){
+        flipTiles();
+      });
+    });
   });
 })(jQuery); // end of jQuery name space
 
@@ -58,17 +73,6 @@ $(".flip-card").flip({
 
 $( document ).ready(function() {
     console.log( "ready!" );
-    $( ".flip-card" ).each(function( index ) {
-      var curtTile = $(this);
-      setTimeout(function(){
-        console.log("set time out triggered");
-        $(curtTile).click();
-        if(index == 6)
-        {
-          randomTiles();
-        }
-      }, (index + 1)*200);
-    });
     var ct = 0;
     var randomTiles = function(){
       setTimeout(function(){
